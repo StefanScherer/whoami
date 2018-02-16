fadc59c6c9a86821bb6692a50947ffbb22ee8fc0
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-17 00:21:29 +0100

    Use docker manifest command from edge channel

--- travis-build.sh
+++ travis-build.sh
@@ -6,6 +6,9 @@ uname -a
 echo "Updating Docker engine to have multi-stage builds"
 sudo service docker stop
 curl -fsSL https://get.docker.com/ | sudo sh
+echo "Enabling docker client experimental features"
+mkdir -p ~/.docker
+echo '{ "experimental": "enabled" }' > ~/.docker/config.json
 docker version
 
 if [ -d tmp ]; then
