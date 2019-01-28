115e9ad6a9c3c426fac0ca1b60aa47a89c3aee81
Stefan Scherer <seal-ss@users.noreply.github.com>
2019-01-28 12:12:25 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -5,6 +5,8 @@ Write-Host Starting build
 if ($isWindows) {
   docker build --pull -t whoami -f Dockerfile.windows .
 } else {
+  uname -a
+  docker version
   docker build -t whoami --build-arg "arch=$env:ARCH" .
 }
 
